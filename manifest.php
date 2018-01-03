fdcb07d07a256afe635e64cc32e09633b1e4a2f4
NTC <trongcong96@gmail.com>
2018-01-03 15:57:02 +0700

    small update

--- manifest.php
+++ manifest.php
@@ -6,12 +6,12 @@ $manifest = array();
 
 $manifest['name']        = __( 'Photography', 'yolo' );
 $manifest['description'] = __( 'This extension adds a fully fledged Photography module to your theme.  ', 'yolo' );
-$manifest['version']     = '1.0.1';
+$manifest['version']     = '1.0.0';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 $manifest['github_repo'] = 'https://github.com/trongcong/Unyson-Photography-Extension';
 $manifest['uri']        = 'http://bearsthemes.com';
 $manifest['author']     = 'Trong Cong';
-$manifest['author_uri'] = 'https://2dev4u.com/';
+$manifest['author_uri'] = 'http://2dev4u.com/';
 
 $manifest['github_update'] = 'trongcong/Unyson-Photography-Extension';
